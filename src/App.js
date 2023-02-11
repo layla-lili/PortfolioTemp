384b4ba8976d8b8865dda61ab165e0f49911f2af
layla-lili <laylaisa.211@gmail.com>
2023-02-12 00:30:33 +0300

    Portfolio template

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Footer, Blog, Possibility, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+import Features from './containers/features/Features';
+import './App.css';
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
